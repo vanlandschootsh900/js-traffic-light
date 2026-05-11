e8c98ab1404d189afe6a26c3efcb0b8abfe3955e
vanlandschootsh900 <vanlandschootsh900@northwested.org>
2026-05-11 09:40:27 -0400

    s

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+// shay VanLandschoot
+// 5/11/26
+// Traffic Light Project
+
+const lights = [
+    document.getElementById('red-light'),
+    document.getElementById('yellow-light'),
+    document.getElementById('green-light'),
+];
+
+let current = 0;
+
+function cycle() {
+    for(let i=0; i< lights.length; i++) {
+        lights[i].className = 'light';
+    }
+
+    lights[current].className = 'light on';
+    
+    current++;
+
+    if (current === 3) {
+        current = 0;
+    }
+}//end fuction
+
+setInterval(cycle, 2000);
+
+cycle();
 
